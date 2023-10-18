645c406a32a3276ada16249eeeaded0a8ee94bb9
placebo889 <hentalia2d@gmail.com>
2023-10-18 10:26:01 +0300

    update

---
+++ js/01-gallery.js
@@ -0,0 +1,43 @@
+import { galleryItems } from "./gallery-items.js";
+// console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = item.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
+  galleryImage.setAttribute("data-source", item.original);
+
+  galleryLink.addEventListener("click", (e) => {
+    e.preventDefault(); 
+    const lightbox = basicLightbox.create(
+      `<img src="${item.original}" alt=item.description>`
+    );
+    lightbox.show(); 
+  });
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+}
+
+function createGallery(items) {
+  const galleryFragment = document.createDocumentFragment();
+  items.forEach((item) => {
+    const galleryItem = createGalleryItem(item);
+    galleryFragment.appendChild(galleryItem);
+  });
+  galleryList.appendChild(galleryFragment);
+}
+
+createGallery(galleryItems);
---
+++ js/02-lightbox.js
@@ -0,0 +1,43 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = item.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+}
+
+function createGallery(items) {
+  const galleryFragment = document.createDocumentFragment();
+  items.forEach((item) => {
+    const galleryItem = createGalleryItem(item);
+    galleryFragment.appendChild(galleryItem);
+  });
+  galleryList.appendChild(galleryFragment);
+}
+
+createGallery(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+  animationSpeed: 50,
+  showCounter: true,
+  captionDelay: 250,
+  preloading: false,
+  alertError: false,
+  captionsData: "alt",
+  fileExt: "png|jpg|jpeg|gif",
+});
